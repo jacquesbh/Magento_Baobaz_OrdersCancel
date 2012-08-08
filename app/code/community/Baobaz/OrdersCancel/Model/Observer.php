2cf1c40077a08e70af76786eb9b66bb3b64a5466
Laurent Clouet <laurent35240@gmail.com>
2012-08-08 23:59:38 +0200

    Credits added

--- app/code/community/Baobaz/OrdersCancel/Model/Observer.php
+++ app/code/community/Baobaz/OrdersCancel/Model/Observer.php
@@ -1,12 +1,12 @@
 <?php
-/*
- * Created on Jun 22, 2009
+/**
  *
- * author : Laurent Clouet
- *
- * www.baobaz.com
- *
-*/
+ * @category   Baobaz
+ * @package    Baobaz_OrdersCancel
+ * @author     Laurent Clouet <laurent35240@gmail.com>
+ * @date       08/08/12
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 class Baobaz_OrdersCancel_Model_Observer {
     private $log_file;
--- app/code/community/Baobaz/OrdersCancel/Model/Order/Statuses.php
+++ app/code/community/Baobaz/OrdersCancel/Model/Order/Statuses.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ *
+ * @category   Baobaz
+ * @package    Baobaz_OrdersCancel
+ * @author     Laurent Clouet <laurent35240@gmail.com>
+ * @date       08/08/12
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 class Baobaz_OrdersCancel_Model_Order_Statuses
 {
        // set null to enable all possible
